44ba046417d95e7959a31dcd013574ec5640c5f3
Koen Schmeets <k.schmeets@gmail.com>
2017-12-09 13:07:20 +0200

    Fixing pkgbuild command

--- src/packager.macos.ts
+++ src/packager.macos.ts
@@ -64,13 +64,16 @@ function pkgbuild(opts) {
     '--version', quote([opts.version]),
     ...(opts.macOs && opts.macOs.identifier ?
       ['--sign', quote([opts.macOs.identifier])] : []),
-    quote([opts.paths.macOsInstallerFile]),
-    ' >'
+    quote([opts.paths.macOsInstallerFile])
   ].join(' ')
-  const pkgbuildCmdResult = execSync(pkgbuildCmd).toString()
-  console.log(pkgbuildCmdResult.split('\n').filter((line) => {
-    return line.indexOf('keychain') === -1
-  }).join('\n'))
+  try {
+    const pkgbuildCmdResult = execSync(pkgbuildCmd).toString()
+    console.log(pkgbuildCmdResult.split('\n').filter((line) => {
+      return line.indexOf('keychain') === -1
+    }).join('\n'))
+  } catch (err) {
+    console.log('Error while creating .pkg')
+  }
 }
 
 function productbuild(opts) {
