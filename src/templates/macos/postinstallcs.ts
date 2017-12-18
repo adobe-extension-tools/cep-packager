a394360622686e76d1ccc72890aae0f8a203143a
Koen Schmeets <k.schmeets@gmail.com>
2017-12-19 02:32:20 +0300

    Fix CS destinations

--- src/templates/macos/postinstallcs.ts
+++ src/templates/macos/postinstallcs.ts
@@ -4,9 +4,15 @@ rm $HOME/.installer-debug-log.txt || true
 echo "Running installer..." > $HOME/.installer-debug-log.txt
 
 // remove if already exists
+rm -rf "/Library/Application Support/Adobe/CS4 ServiceManager/extensions/${opts.bundleId}" || true
+rm -rf "/Library/Application Support/Adobe/CS5 ServiceManager/extensions/${opts.bundleId}" || true
+rm -rf "/Library/Application Support/Adobe/CS5.5 ServiceManager/extensions/${opts.bundleId}" || true
 rm -rf "/Library/Application Support/Adobe/CS6 ServiceManager/extensions/${opts.bundleId}" || true
 
 // install extension
+unzip ./bundle.zxp -d "/Library/Application Support/Adobe/CS4 ServiceManager/extensions/${opts.bundleId}" >> $HOME/.installer-debug-log.txt 2>&1
+unzip ./bundle.zxp -d "/Library/Application Support/Adobe/CS5 ServiceManager/extensions/${opts.bundleId}" >> $HOME/.installer-debug-log.txt 2>&1
+unzip ./bundle.zxp -d "/Library/Application Support/Adobe/CS5.5 ServiceManager/extensions/${opts.bundleId}" >> $HOME/.installer-debug-log.txt 2>&1
 unzip ./bundle.zxp -d "/Library/Application Support/Adobe/CS6 ServiceManager/extensions/${opts.bundleId}" >> $HOME/.installer-debug-log.txt 2>&1
 
 echo "Done." >> $HOME/.installer-debug-log.txt
