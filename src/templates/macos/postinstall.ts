ee503ccdf942f47eee5222425c159f41e758d01a
Koen Schmeets <hello@koenschmeets.nl>
2019-12-12 14:15:47 +0100

    Testing spaces

--- src/templates/macos/postinstall.ts
+++ src/templates/macos/postinstall.ts
@@ -1,15 +1,15 @@
 export default (opts) => `#!/bin/bash
-LOG_FILE="$HOME/.installer-debug-log.txt"
-rm -f $LOG_FILE || true
-echo "Uninstalling previous versions if any..." >> $LOG_FILE
-./ExManCmd_mac/Contents/MacOS/ExManCmd --remove ${opts.bundleId} || true >> $LOG_FILE 2>&1
-echo "Installing new versions..." >> $LOG_FILE
-./ExManCmd_mac/Contents/MacOS/ExManCmd --install ./bundle.zxp >> $LOG_FILE 2>&1
+LOG_FILE="$HOME/test ing/.installer-debug-log.txt"
+rm -f "$LOG_FILE" || true
+echo "Uninstalling previous versions if any..." >> "$LOG_FILE"
+./ExManCmd_mac/Contents/MacOS/ExManCmd --remove ${opts.bundleId} || true >> "$LOG_FILE" 2>&1
+echo "Installing new versions..." >> "$LOG_FILE"
+./ExManCmd_mac/Contents/MacOS/ExManCmd --install ./bundle.zxp >> "$LOG_FILE" 2>&1
 if grep -q Failed "$LOG_FILE"; then
-    echo "Installation failed, please look up the error code above on the following website:\nhttps://helpx.adobe.com/exchange/kb/error-codes.html\nOpening the log file and website." >> $LOG_FILE
-    open $LOG_FILE
+    echo "Installation failed, please look up the error code above on the following website:\nhttps://helpx.adobe.com/exchange/kb/error-codes.html\nOpening the log file and website." >> "$LOG_FILE"
+    open "$LOG_FILE"
     open https://helpx.adobe.com/exchange/kb/error-codes.html
-    echo "If you still experience issues, please contact us." >> $LOG_FILE
+    echo "If you still experience issues, please contact us." >> "$LOG_FILE"
     exit 1
 fi
-echo "Done." >> $LOG_FILE`
+echo "Done." >> "$LOG_FILE"`
